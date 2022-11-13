bb3e077ba4a28331cde48b3dc0552dbda180e993
raivis-rasnacs-jak <113161473+raivis-rasnacs-jak@users.noreply.github.com>
2022-11-13 13:06:05 +0200

    Create test_math.py

---
+++ test_math.py
@@ -0,0 +1,13 @@
+import math
+
+"""
+Testi uzdevumam
+"""
+
+def test_saskaita():
+  assert math.saskaita(2, 3) == 5
+  assert math.saskaita(6, 7) == 13
+  
+def test_reizina():
+  assert math.reizina(1, 3) == 3
+  assert math.reizina(3, 5) == 15
